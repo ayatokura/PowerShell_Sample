639bbb2f526c667106215d9e1a38c6132adad5fe
ayatokura <ayayu@live.jp>
2017-03-31 11:13:48 +0900

    first commit

---
+++ InfiniteLoopTime.ps1
@@ -0,0 +1,14 @@
+﻿# PowerShellを試してみるためのサンプルスクリプトです。
+# これを実行すると強制終了するまで30秒毎に日付と時間を表示します。
+
+
+# 無限ループ
+# 強制終了されるまで、永遠に処理を繰り返します。
+while(1) {
+
+    # メッセージを表示します。
+    Write-Output ('日時：' + (Get-Date -format yyyyMMdd_HHmmss)) 
+
+    # 30秒待機します。
+    Start-Sleep -Seconds 30
+}
